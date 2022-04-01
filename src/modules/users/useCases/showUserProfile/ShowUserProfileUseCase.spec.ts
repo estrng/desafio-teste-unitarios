3abd69f32b0646212e872e7d672d55c776293006
José Ivan R. de Oliveira <ivan_r.o@hotmail.com>
2022-03-31 22:48:18 -0300

    desafio feito

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Show User Profile", () => {
+  beforeEach(async () => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    showUserProfileUseCase = new ShowUserProfileUseCase(
+      inMemoryUsersRepository
+    );
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+  it("should be able to show user profile ", async () => {
+    const user = await createUserUseCase.execute({
+      name: "jose",
+      email: "jose@gmail.com",
+      password: "123123",
+    });
+
+    if (user.id) {
+      const response = await showUserProfileUseCase.execute(user.id);
+
+      expect(response).toHaveProperty("id");
+    }
+  });
+});
